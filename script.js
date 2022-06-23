1f01971a03cf8416712118106baab301d264128a
Your Name <you@example.com>
2022-06-23 11:54:24 +0500

    end

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ document.querySelector("#btn").addEventListener("click", function() {
     myText();
 })
 
+function myText(){
+    let message, text; 
+    let errElement = document.getElementById("text");
+    message = document.getElementById("err");
+    text = document.getElementById("text").value;
+
+    
+    try{
+        if(text == ""){
+            errElement.classList.remove("valid");
+            errElement.classList.add("err");
+            throw "bo`sh";
+        }
+
+        if(text.lenght > 0){
+            errElement.classList.remove("err");
+            errElement.classList.add("valid")
+            message.innerHTML = "";
+        }
+    }
+
+    catch(e){
+        
+    }
+}
+
+
+
